727eb19207b7b62b64ec85c0ae509787c569c1f7
btylk <55312210+btylk@users.noreply.github.com>
2019-09-21 23:37:09 +0700

    update shuffle

--- CharacterCard.js
+++ CharacterCard.js
@@ -3,9 +3,14 @@ export default class CharacterCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      active: false
-    };
+      active: false,
+    }
   }
+  componentDidUpdate(prevProps){
+    if(prevProps.attempt != this.props.attempt){
+    this.setState({active: false})
+    }
+   } 
 
   activate = () => {
     if (!this.state.active) {
--- WordCard.js
+++ WordCard.js
@@ -1,12 +1,42 @@
 import CharacterCard from './CharacterCard';
+import './App.css';
 import React, { Component } from 'react';
+import _ from 'lodash';
+
+const prepareStateFromWord = (given_word) => {
+    let word = given_word.toUpperCase()
+    let chars = _.shuffle(Array.from(word))
+    return {
+        word,
+        chars,
+        attempt: 1,
+        guess: [],
+        completed: false
+    }
+}
 export default class WordCard extends
 Component {
-    activationHandler = c => { console.log(`${c} has been activated.`) }
+    constructor(props) {
+        super(props)
+        this.state = prepareStateFromWord(this.props.value)
+        }
+    activationHandler = c => {
+        console.log(`${this.state.word}`)
+        let guess = [...this.state.guess, c]
+        this.setState({guess})
+        if(guess.length == this.state.chars.length){
+            if(guess.join('').toString() == this.state.word){
+                this.setState({guess: [], completed: true})
+            }else{
+                let chars = _.shuffle(Array.from(this.state.word))
+                this.setState({guess: [],chars : chars, attempt: this.state.attempt + 1})
+            }
+        }
+    }
     render(){
         return(
             <div>
-                { Array.from(this.props.value).map((c,i) => <CharacterCard value={c} key={i} activationHandler={this.activationHandler}/>)}
+                { Array.from(this.state.chars).map((c,i) => <CharacterCard value={c} key={i} activationHandler={this.activationHandler} attempt={this.state.attempt}/>)}
             </div>
 
         );
