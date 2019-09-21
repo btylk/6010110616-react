9f039241e93478c0c5dbd107d67bbb8e78446aee
btylk <55312210+btylk@users.noreply.github.com>
2019-09-22 02:05:36 +0700

    add word

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import CharacterCard from './CharacterCard';
 import './App.css';
 import React, { Component } from 'react';
 import WordCard from './WordCard';
-const word = "Hello";
+var vocab = ["react","chocolatey","yarn"]
+let randomIndex = parseInt(Math.random() * 3)
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <WordCard value="react"/>
+        <WordCard value={vocab[randomIndex]}/>
       </div>
-
     );
   }
 }
